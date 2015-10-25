78d37f733d6ee7b8652de2eca60573762f9ee6c5
nicolele <niwo8042@colorado.edu>
2015-10-25 16:46:13 -0600

    spotify search for a song returns artists, track id, track name

--- backend/song.py
+++ backend/song.py
@@ -5,6 +5,19 @@ def song_lookup(name=None, artist=None, limit=1):
     results = sp.search(q='track:' + name, 
     	type='track', 
     	limit=limit)
-    return results
+    if len(results['tracks']['items']) == 0:
+    	return "No results found for " + song
+    else:
+    	songs = {}
+    	artists = results['tracks']['items'][0]['artists']
+    	artist_names = []
+    	for index, names in enumerate(artists):
+    		artist_names.append(names['name'])
+    	songs['artists'] = artist_names
+    	songs['trackid'] = results['tracks']['items'][0]['id']
+    	songs['track'] = results['tracks']['items'][0]['name']
+    	print("Results for " + songs['track'] + ' by ' + songs['artists'][0])
+    	return songs
 
-print(song_lookup('Diamonds From Sierra Leone - Remix - Album Version (Explicit)'))
+song_lookup('Diamonds From Sierra Leone - Remix - Album Version (Explicit)')
+#print(song_lookup('asdlkajsdlkajslaksjdlaksjdalksjdalksjd'))
