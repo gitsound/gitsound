f97f60e1ae83b9799c8e9e765fa17d153bfc4489
mguida22 <michael.guida@colorado.edu>
2015-11-05 13:44:07 -0700

    Begin to hook in cli
    Connects remove, add and show remote

--- backend/cli.py
+++ backend/cli.py
@@ -19,8 +19,30 @@ Command List:
 """
 
 from docopt import docopt
+import gitSound
+import json
+import os
+
+def login():
+    askText = "please enter your spotify username: "
+    username = input(askText)
+
+    if os.path.isfile("gitSound_config.txt") == False:
+        raise RuntimeError(
+            "cannot find gitSound_config.txt in the working directory")
+
+    with open("gitSound_config.txt") as configFile:
+        configVars = json.loads(configFile.read())
+
+    newUser = gitSound.spotifyUser(
+        username, configVars["client_id"], configVars["client_secret"],
+        "http://localhost/callback")
+
+    return newUser
+
 
 if __name__ == '__main__':
+
     # Initialize docopt and grab args
     args = docopt(__doc__)
 
@@ -28,25 +50,42 @@ if __name__ == '__main__':
     cmd = args['<command>']
     arg = args['<argument>']
 
+    user = login()
+    currentPID = user.getPlaylistIDs()[6]
+
     # Determine how to handle args
     if (cmd == 'show'):
         if (arg == 'local'):
+            print('Not yet implemented.')
             print('Show all local playlists')
         elif (arg == 'remote'):
-            print('Show all remote playlists')
+            print(user.getPlaylistIDs())
         else:
+            print('Not yet implemented.')
             print('Show all playlists, local and remote')
     elif (cmd == 'select' and arg != None):
+        print('Not yet implemented')
         print('Select playlist with id ' + arg)
     elif (cmd == 'clone' and arg != None):
+        print('Not yet implemented.')
         print('Clone playlist with id ' + arg)
     elif (cmd == 'add' and arg != None):
-        print('Add track with id ' + arg + ' to current playlist')
+        if (not currentPID):
+            print('Select a playlist first')
+        else:
+            user.addSongToPlaylist(currentPID, arg)
+            print('Add track with id ' + arg + ' to current playlist')
     elif (cmd == 'remove' and arg != None):
-        print('Remove track with id ' + arg + ' from current playlist')
+        if (not currentPID):
+            print('Select a playlist first')
+        else:
+            user.removeSongFromPlaylist(currentPID, arg)
+            print('Remove track with id ' + arg + ' from current playlist')
     elif (cmd == 'commit'):
+        print('Not yet implemented.')
         print('Commit all changes to current playlist')
     elif (cmd == 'status'):
+        print('Not yet implemented.')
         print('Show changes to commit')
     else:
         print('Usage: cli.py <command> [<argument>]')
